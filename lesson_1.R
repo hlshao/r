b3f8c6c13a91f141f0083ac9f73a1944323aaa03
hlshao <hlshao2@gmail.com>
2018-09-10 12:49:53 +0800

    Update lesson_1.R

--- lesson_1.R
+++ lesson_1.R
@@ -7,35 +7,18 @@ rm(list = ls())
 data=iris
 
 # way 2, read from CSV (Comma-Separated Values)
-data_csv=read.csv("iris_csv.csv",sep = ",",header = T)
-
-#read.table(file, header = FALSE, sep = "", quote = "\"'",
-#           dec = ".", numerals = c("allow.loss", "warn.loss", "no.loss"),
-#           row.names, col.names, as.is = !stringsAsFactors,
-#           na.strings = "NA", colClasses = NA, nrows = -1,
-#           skip = 0, check.names = TRUE, fill = !blank.lines.skip,
-#           strip.white = FALSE, blank.lines.skip = TRUE,
-#           comment.char = "#",
-#           allowEscapes = FALSE, flush = FALSE,
-#           stringsAsFactors = default.stringsAsFactors(),
-#           fileEncoding = "", encoding = "unknown", text, skipNul = FALSE)
+data_csv=read.csv
 
 # way 3, read from xlsx
-## install.packages("xlsx")
-# options(java.home="C:\\Program Files\\Java\\jdk1.8.0_91\\jre")
-library(xlsx)
-data_xlsx=read.xlsx("iris.xlsx",sheetIndex = 1)
+## 請跳過第一種方法 library(xlsx)
+## 請跳過第一種方法 data_xlsx=read.xlsx("iris.xlsx",sheetIndex = 1)
 
-#read.xlsx(file, sheetIndex, sheetName=NULL, rowIndex=NULL,
-#            startRow=NULL, endRow=NULL, colIndex=NULL,
-#            as.data.frame=TRUE, header=TRUE, colClasses=NA,
-#            keepFormulas=FALSE, encoding="unknown", password=NULL, ...)
-
+#請從這裡繼續
 # way 3-1, read from xlsx
 library(readxl)
 data_xlsx2=read_xlsx("iris.xlsx")
 
-#read_xlsx(path, sheet = NULL, range = NULL, col_names = TRUE,
+#提示 read_xlsx(path, sheet = NULL, range = NULL, col_names = TRUE,
 #          col_types = NULL, na = "", trim_ws = TRUE, skip = 0, n_max = Inf,
 #          guess_max = min(1000, n_max))
 
