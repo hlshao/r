57cb97710ece31d87ea7ef741544e203b08a7bea
hlshao <hlshao2@gmail.com>
2018-09-10 12:55:58 +0800

    Update lesson_2.R

--- lesson_2.R
+++ lesson_2.R
@@ -7,40 +7,34 @@ data_i=iris
 
 ## 1. basic plot
 # scatter plot
-plot(data_i$Sepal.Length,data_i$Sepal.Width)
-plot(data_i$Sepal.Length,data_i$Sepal.Width,col="red")
-plot(data_i$Sepal.Length,data_i$Sepal.Width,col=data_i$Species)
-plot(data_i$Sepal.Length,data_i$Sepal.Width,pch=as.numeric(data_i$Species))
-# for paper print
-plot(data_i$Sepal.Length,data_i$Sepal.Width,col=data_i$Species,main="花萼長寬比")
-plot(data_i$Sepal.Length,data_i$Sepal.Width,type="l")
+plot(DO)
 
-?plot
+# for paper print
+plot(DO)
 
 #add line 
 # 我們也可以畫出回歸趨勢線
-lm.model <- lm(Sepal.Width ~ Sepal.Length, data_i)    # 建立一個線性回歸
+lm.model <- lm(DO)    # 建立一個線性回歸
 # 畫上回歸的趨勢線
-abline(lm.model,lwd=2)     # lwd 代表線的粗細
+abline(DO)     # lwd 代表線的粗細
 
 #add curve
-M.Loess <- loess(Sepal.Width ~ Sepal.Length, data = data_i)
-Fit <- fitted(M.Loess)
-Ord1 <- order(data_i$Sepal.Length)
-lines(data_i$Sepal.Length[Ord1], Fit[Ord1],
-      lwd = 3, lty = 2)
+M.Loess <- loess(DO)
+Fit <- fitted(DO)
+Ord1 <- order(DO)
+lines(DO)
 
 # Advanced scatter plot
-plot(data)
-plot(data[1:4])
+plot(DO)
+plot(data[DO])
 
 # function plot
-plot(sin, -pi, 2*pi)
+plot(sin, DO)
 
 #hist
-hist(data_t$Sepal.Length)
-hist(data_t$Sepal.Length,breaks = 50)
-#hist(x, breaks = "Sturges",
+hist(DO)
+
+#提示hist(x, breaks = "Sturges",
 #     freq = NULL, probability = !freq,
 #     include.lowest = TRUE, right = TRUE,
 #     density = NULL, angle = 45, col = NULL, border = NULL,
@@ -51,8 +45,9 @@ hist(data_t$Sepal.Length,breaks = 50)
 #     nclass = NULL, warn.unused = TRUE, ...)
 
 #boxplot
-boxplot(data_i[1:4])
-boxplot(Sepal.Length ~ Species, data_i)
+boxplot(data_i[DO])
+boxplot(DO, data_i)
+
 #最大值區間：Q3 + 1.5 * (Q3-Q1)，以橫線表示
 #上四分位數Q3：以內箱上限表示
 #中位數：以內箱中的黑粗線表示
@@ -61,7 +56,7 @@ boxplot(Sepal.Length ~ Species, data_i)
 #離群值：超過最大值區間或最小值區間的樣本，以圈圈表示
 
 # output
-png("boxplot.png", width=600, height=600)
+png(DO)
 dev.off() 
 
 # homework
